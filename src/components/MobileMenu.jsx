d1093935dc30e101fc3f6b0646ba8207374f56b5
Yasmeen-Saad <yasmeensaad626@gmail.com>
2025-04-23 00:43:58 +0200

    enhance responsive

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,6 +1,6 @@
 export const MobileMenu = ({menuOpen, setMenuOpen}) => {
     return (
-        <div className={`fixed top-0 left-0 w-full bg-[rgba(10, 10, 10, 0.8)] z-40 flex flex-col items-center justify-center transition-all 
+        <div className={`fixed top-0 left-0 w-full bg-black/80 z-40 flex flex-col items-center justify-center transition-all 
             duration-300 ease-in-out md:hidden ${menuOpen ? "h-screen opacity-100 pointer-events-auto" : "h-0 opacity-0 pointer-events-none"}`
         }>
             <button 
